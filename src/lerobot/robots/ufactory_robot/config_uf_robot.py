2818e0afa30d0251a70a2989dcdaa900a5a24856
vinman <vinman.cub@gmail.com>
2026-01-29 18:41:52 +0800

    [fix] (ufactory_robot) fix parameters and support xarm5/6/7
    1. add parameter start_joints
    2. fix camera parameters
    3. support xarm5/6/7

--- src/lerobot/robots/ufactory_robot/config_uf_robot.py
+++ src/lerobot/robots/ufactory_robot/config_uf_robot.py
@@ -1,5 +1,6 @@
 from dataclasses import dataclass, field
-
+from typing import Tuple
+import numpy as np
 from lerobot.cameras import CameraConfig
 from lerobot.cameras.opencv import OpenCVCameraConfig
 from lerobot.cameras.realsense import RealSenseCameraConfig
@@ -13,17 +14,17 @@ class UFRobotConfig(RobotConfig):
     cameras: dict[str, CameraConfig] = field(
         default_factory=lambda: {
             "overhead": RealSenseCameraConfig(
-                serial_number_or_name="Intel RealSense D435",
+                serial_number_or_name="Intel RealSense D435I",
                 fps=30,
-                width=1280,
-                height=720,
+                width=640, # 1280
+                height=480, # 720
                 # rotation=90,
             ),
             "tool": RealSenseCameraConfig(
-                serial_number_or_name="Intel RealSense D435I",
+                serial_number_or_name="Intel RealSense D435",
                 fps=30,
-                width=1280,
-                height=720,
+                width=640, # 1280
+                height=480, # 720
             ),
         }
     )
@@ -33,3 +34,4 @@ class UFRobotConfig(RobotConfig):
     control_space: str = "joint"
     gripper_control: bool = True
     observe_joint_vel: bool = False # only effective in joint control mode
+    start_joints: Tuple[float, ...] = (0, 0, 0, np.pi/2, 0, np.pi/2, 0)
